94974cf45ed1652bc1cdcc5f914db3276419c5a0
Jyoti <kumarijyoti6432@gmail.com>
2026-06-12 18:21:01 +0530

    added auth0 provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,19 +12,29 @@ import { Toaster } from "react-hot-toast";
 import { GlobalContextProvider } from "./context/GlobalContext";
 import { JobsContextProvider } from "./context/JobsContext";
 
+import { Auth0Provider } from "@auth0/auth0-react";
+
 // Font Awesome
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <GlobalContextProvider>
-        <JobsContextProvider>
-          <Toaster position="top-center" />
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <BrowserRouter>
+        <GlobalContextProvider>
+          <JobsContextProvider>
+            <Toaster position="top-center" />
 
-          <App />
-        </JobsContextProvider>
-      </GlobalContextProvider>
-    </BrowserRouter>
+            <App />
+          </JobsContextProvider>
+        </GlobalContextProvider>
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>
 );
