5127ceec3de5ce00dc9ef9391c8e4b0a3e23fa32
Jyoti <kumarijyoti6432@gmail.com>
2026-06-13 14:13:02 +0530

    Add Auth0 isLoading check to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import Home from "./pages/Home";
 import Finder from "./pages/Finder";
 import MyJob from "./pages/MyJob";
@@ -6,6 +7,10 @@ import Post from "./pages/Post";
 import JobDetails from "./pages/JobDetails";
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div style={{display:'flex', justifyContent:'center', marginTop:'2rem'}}>Loading...</div>;
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
