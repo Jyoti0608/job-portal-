c8108bf7e73e05e46ac5c82e13426188222d5fae
Jyoti <kumarijyoti6432@gmail.com>
2026-06-14 13:17:18 +0530

    Add tokenSigningAlg RS256 back to protect middleware

--- server/middleware/protect.js
+++ server/middleware/protect.js
@@ -3,6 +3,7 @@ import { auth } from "express-oauth2-jwt-bearer";
 const protect = auth({
   audience: "https://job-portal-h2e0.onrender.com",
   issuerBaseURL: "https://dev-gfgs5zwhuz3mwhd5.us.auth0.com",
+  tokenSigningAlg: "RS256",
 });
 
 export default protect;
