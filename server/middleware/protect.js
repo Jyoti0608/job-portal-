2f6f381cc62ab517243ede45b4e774629d9c899a
Jyoti <kumarijyoti6432@gmail.com>
2026-06-14 12:01:31 +0530

    Fix protect middleware: remove tokenSigningAlg for RS256

--- server/middleware/protect.js
+++ server/middleware/protect.js
@@ -3,7 +3,6 @@ import { auth } from "express-oauth2-jwt-bearer";
 const protect = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.ISSUER_BASE_URL,
-  tokenSigningAlg: "RS256",
 });
 
 export default protect;
